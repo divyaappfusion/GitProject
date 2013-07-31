e0bab3478a4f7daf220a756157404f10993a1805
Prashant Vashisht <prashantvashisht@Prashants-MacBook-Pro.local>
2013-07-31 12:33:34 +0530

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Prashant Vashisht on 7/31/13.
+//  Copyright (c) 2013 Prashant Vashisht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Prashant Vashisht on 7/31/13.
+//  Copyright (c) 2013 Prashant Vashisht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
