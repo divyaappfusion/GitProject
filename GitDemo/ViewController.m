05e380e7f1b4a9c14c6ad65863d16a11c1cfefde
divya <divya@mail.com>
2013-07-31 15:21:20 +0530

    change background color of view comtroller

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -18,12 +18,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning
 {
     
-   // NSLoadedClasses Testing
+   // NSLoadedClasses
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
